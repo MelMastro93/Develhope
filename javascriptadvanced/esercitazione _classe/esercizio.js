cc22da56791a27285ea88b0dcf43e69f2848af4e
Melissa Mastrovincenzo <melissa.mastrovincenzo@gmail.com>
2023-11-21 16:44:48 +0100

    esercitazione for array

--- javascriptadvanced/esercitazione _classe/esercizio.js
+++ javascriptadvanced/esercitazione _classe/esercizio.js
@@ -16,8 +16,48 @@ let users = [
 
 ]
 
+
+
+// let htmlUsers = users.filter(user => user.languages.includes("html"))
+// console.log(htmlUsers)
+
+// let htmlUsers = []
+// for(let i = 0 ; i < users.length ; i++ ){
+//     if(users[i].languages.includes("html"))
+//     htmlUsers.push(users[i])
+// }
+// console.log(htmlUsers)
+
+
+
 //let loop = users.forEach(e => console.log(e.name));
 
-for(let i = 0 ; i < users.length ; i ++){
-    console.log(users[i].nome);
-}
+// for(let i = 0 ; i < users.length ; i ++){
+//     console.log(users[i].nome);
+// }
+
+// const nickName = users.map(ciao =>'${ciao.name.toLowerCase()} . ${ciao.surname.toLowerCase()}')
+// console.log(nickName)
+
+// let nickNames= []
+// for(let x = 0; x < users.length; x++ ){
+//     let nickName = '${users[x].name.toLowerCase}.${users[x].surname.toLowerCase}
+//     nickNames.push(nickName)
+// }
+
+// console.log(nickName)
+
+// const peopleWitha = users.find(user => user.name.includes("r"));
+// console.log(peopleWitha);
+
+// const peopleWitha = []
+// for(let i = 0; i < users.length; i++){
+//     if (users[i].name.includes("r")){
+//         peopleWitha.push(users[i]);
+//         console.log(peopleWitha)
+//     }
+
+// }
+// console.log(peopleWitha);
+
+
