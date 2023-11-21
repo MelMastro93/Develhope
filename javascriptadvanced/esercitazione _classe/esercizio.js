27df7554fc2ec59010e1fea9f32f36b4756136df
Melissa Mastrovincenzo <melissa.mastrovincenzo@gmail.com>
2023-11-21 15:29:28 +0100

    ciclo for

---
+++ javascriptadvanced/esercitazione _classe/esercizio.js
@@ -0,0 +1,23 @@
+let users = [
+    {   id: 1,
+        name: "Mario",
+        surname:"Rossi",
+        age: 35,
+        languages: ["html", "css", "javascript"]
+
+    },
+    {   id: 1,
+        name: "Franco",
+        surname:"Verdi",
+        age: 34,
+        languages: ["python", "javascript"]
+
+    },
+
+]
+
+//let loop = users.forEach(e => console.log(e.name));
+
+for(let i = 0 ; i < users.length ; i ++){
+    console.log(users[i].nome);
+}
