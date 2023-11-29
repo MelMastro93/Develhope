70d8aefb1979329e3cf77baece0b25ab96fbcfad
Melissa Mastrovincenzo <melissa.mastrovincenzo@gmail.com>
2023-11-29 22:02:01 +0100

    setTimeout added

--- javascriptadvanced/array_methods/set_timeout4/exercise.js
+++ javascriptadvanced/array_methods/set_timeout4/exercise.js
@@ -6,3 +6,11 @@
 // Prima dello scadere dei 2 secondi stampa in console il messaggio ""Lancio dei dadi in corso..."
 //Per la generazione casuale dei numeri dei dadi servirà utilizzare i metodi Math.floor() e Math.random()
 
+function lanciaDadi(dado1,dado2){
+    
+    dado1 = Math.floor(Math.random() * 6);
+    dado2 = Math.floor(Math.random()* 6);
+    console.log(`Dado 1 = ${dado1}, Dado 2 = ${dado2}`);
+}
+console.log("Lancio dei dadi in corso...")
+setTimeout(lanciaDadi,2000)
