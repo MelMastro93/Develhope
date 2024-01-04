fcd5a3e36175b43e2ea22115363f41ce6b26c450
Melissa Mastrovincenzo <148075905+MelMastro93@users.noreply.github.com>
2024-01-04 14:52:38 +0100

    Math.random modificato

--- javascriptadvanced/mondo_asincorono/promise/promise4/exercise.js
+++ javascriptadvanced/mondo_asincorono/promise/promise4/exercise.js
@@ -4,12 +4,15 @@
 // -La chiamata avrà successo con il messaggio Data retrieved successfully se il 
 // valore random è inferiore a 0.5 altrimenti fallisce con il messaggio Error: Failed to fetch data
 
+/* APPUNTO : precedentemente avevo generato il Math.random() moltiplicandolo * 0.5. In questo modo mi cadeva sempre nel resolve
+considerando che il Math.random genera un numero tra 0 e 1 ho tolto il *0.5 e creando direttamente la condizione.
+ */
 
 function fetchDataFromAPI() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            let success = Math.random() * 0.5
-            if (success <= 0.6) {
+            let success = Math.random()
+            if (success <= 0.5) {
                 resolve(`Data retrieved successfully: ${(success)}`)
             } else reject(new Error("Failed to fetch data"))
         },2000)
